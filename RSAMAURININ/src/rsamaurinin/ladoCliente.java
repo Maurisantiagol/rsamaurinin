ca4bb4c9e4afcabca8f21c1b5c94067f94c6a79e
maurisantiagol <msantiagod1901@alumno.ipn.mx>
2021-11-04 15:21:31 -0600

    rsa mano

--- RSAMAURININ/src/rsamaurinin/ladoCliente.java
+++ RSAMAURININ/src/rsamaurinin/ladoCliente.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chat;
+package rsamaurinin;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -226,6 +226,7 @@ public class ladoCliente extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(ladoCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
--- RSAMAURININ/src/rsamaurinin/ladoServidor.java
+++ RSAMAURININ/src/rsamaurinin/ladoServidor.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chat;
+package rsamaurinin;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -223,6 +223,7 @@ public class ladoServidor extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(ladoServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
